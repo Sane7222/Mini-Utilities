bea5421f8dcfe151334ecd7e77a9ec55da0d4baf
Bradley Dowling <bradley.dowling@wsu.edu>
2023-02-06 19:05:52 -0800

    Update my-sed.c

--- my-sed.c
+++ my-sed.c
@@ -16,9 +16,24 @@ void main(int argc, char *argv[]) {
 
     if (argc < 4) {
         // need to read from STDIN...
+        exit(0);
     }
 
-    // to be continued...
+    // open the file
+    FILE *fp = fopen(argv[3], "r");
+    if (fp == NULL) {
+        printf("cannot open the file \n");
+        exit(1);
+    }
+
+    // read a line
+    char *line = NULL;
+    size_t len = 0;
+    size_t characters;
+    
+    characters = getline(&line, &len, fp);
+    printf("%s", line);
+    fclose(fp);
 
     exit(0);
 }
